454b5a6acbf245fcbe35660072e99d807c34cb95
Markus Felten <Markus.Felten@gmx.de>
2018-10-07 13:39:47 +0200

    test: try to remove branches

--- tests/repository-test.js
+++ tests/repository-test.js
@@ -40,14 +40,15 @@ test("create commit", async t => {
 
   const newName = `commit-test-${branches.size}`;
   const branch = await repository.createBranch(newName);
+  try {
+    const commit = await branch.commit("message text", [
+      new Content("README.md", `file content #${branches.size}`)
+    ]);
 
-  const commit = await branch.commit("message text", [
-    new Content("README.md", `file content #${branches.size}`)
-  ]);
-
-  t.is(commit.ref, `refs/heads/${newName}`);
-
-  await repository.deleteBranch(newName);
+    t.is(commit.ref, `refs/heads/${newName}`);
+  } finally {
+    await repository.deleteBranch(newName);
+  }
 });
 
 test("list files", async t => {
@@ -94,5 +95,8 @@ test("missing content", async t => {
   const provider = new GithubProvider(config);
   const repository = await provider.repository(REPOSITORY_NAME);
   const branch = await repository.branch("master");
-  await t.throwsAsync(async () => branch.content("missing/file"), { instanceOf: Error , message: "Not Found" })
+  await t.throwsAsync(async () => branch.content("missing/file"), {
+    instanceOf: Error,
+    message: "Not Found"
+  });
 });
