3cbe0941db3b45bd9e42829350509d90bf1dbf42
Markus Felten <Markus.Felten@gmx.de>
2019-05-19 00:01:05 +0200

    test: check for foreign owned repositories

--- tests/list-repositories-test.mjs
+++ tests/list-repositories-test.mjs
@@ -16,6 +16,19 @@ test("list repositories", async t => {
   t.truthy(Object.keys(repositories).length > 3);
 });
 
+test("list repositories non owner", async t => {
+  const provider = new GithubProvider(config);
+
+  const repositories = {};
+
+  for await (const r of provider.repositories("k0nsti/konsum*")) {
+    repositories[r.fullName] = r;
+  }
+
+  t.truthy(Object.keys(repositories).length > 1);
+  t.truthy(repositories['k0nsti/konsum'].name, 'konsum');
+});
+
 test("list repositories negative group", async t => {
   const provider = new GithubProvider(config);
 
@@ -24,6 +37,24 @@ test("list repositories negative group", async t => {
   for await (const r of provider.repositories("xtzrtrhtl/npm-*")) {
     repositories[r.name] = r;
   }
+
+  t.truthy(Object.keys(repositories).length === 0);
+});
+
+
+test.skip("list repositories all", async t => {
+  const provider = new GithubProvider(config);
+
+  for await (const g of provider.repositoryGroups()) {
+    console.log(g);
+  }
+
+  const repositories = {};
+
+  for await (const r of provider.repositories("*/*")) {
+    repositories[r.name] = r;
+  }
+  console.log(Object.keys(repositories));
 
   t.truthy(Object.keys(repositories).length === 0);
 });
