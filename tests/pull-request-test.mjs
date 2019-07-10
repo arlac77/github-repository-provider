550f57d5bd7f21cb7795d50b32cbf9d70601e79a
Markus Felten <Markus.Felten@gmx.de>
2019-07-10 10:37:45 +0200

    test: make test pass

--- tests/pull-request-test.mjs
+++ tests/pull-request-test.mjs
@@ -7,19 +7,19 @@ const REPOSITORY_NAME = "arlac77/sync-test-repository";
 
 test("pull requests list", async t => {
   const provider = GithubProvider.initialize(undefined, process.env);
-  const repository = await provider.repository(
-    REPOSITORY_NAME + "#some-other-branch"
-  );
+  const repository = await provider.repository(REPOSITORY_NAME);
 
   for await (const pr of repository.pullRequests()) {
-   // t.is(pr.destination, await repository.defaultBranch);
-    t.true(pr.name.length >= 1);
+    t.is(pr.destination, await repository.defaultBranch);
+    t.true(pr.number !== undefined);
     t.true(pr.title.length >= 1);
    // t.truthy(pr.title.match(/merge package template/));
     t.is(pr.state, ["MERGED","OPEN"].find(s => s === pr.state));
     //t.false(pr.merged);
     t.false(pr.locked);
   }
+
+  t.true(repository !== undefined);
 });
 
 test("pull requests create decline", async t => {
