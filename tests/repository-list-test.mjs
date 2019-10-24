a372ec4d405f17abdf728c4b82030516b8d7dbda
Markus Felten <Markus.Felten@gmx.de>
2019-10-24 14:00:22 +0200

    test: make use of repositoryListTest

--- tests/repository-list-test.mjs
+++ tests/repository-list-test.mjs
@@ -1,19 +1,12 @@
 import test from "ava";
+import { repositoryListTest } from "repository-provider-test-support";
+
 import { GithubProvider } from "../src/github-provider.mjs";
 
 const config = GithubProvider.optionsFromEnvironment(process.env);
 
-test("list repositories", async t => {
-  const provider = new GithubProvider(config);
-
-  const repositories = {};
-
-  for await (const r of provider.repositories("arlac77/npm-*")) {
-    repositories[r.name] = r;
-  }
-
-  //console.log(Object.keys(repositories));
-  t.truthy(Object.keys(repositories).length > 3);
+test.only(repositoryListTest, new GithubProvider(config), "arlac77/npm-*", {
+  "npm-template-sync": { name: "npm-template-sync" }
 });
 
 test("list repositories more", async t => {
@@ -29,7 +22,6 @@ test("list repositories more", async t => {
   t.truthy(Object.keys(branches).length > 50);
 });
 
-
 test("list repositories non owner", async t => {
   const provider = new GithubProvider(config);
 
@@ -40,7 +32,7 @@ test("list repositories non owner", async t => {
   }
 
   t.truthy(Object.keys(repositories).length > 1);
-  t.truthy(repositories['k0nsti/konsum'].name, 'konsum');
+  t.truthy(repositories["k0nsti/konsum"].name, "konsum");
 });
 
 test("list repositories negative group", async t => {
@@ -55,12 +47,10 @@ test("list repositories negative group", async t => {
   t.truthy(Object.keys(repositories).length === 0);
 });
 
-
 test.skip("list repositories all", async t => {
   const provider = new GithubProvider(config);
 
-
-//_initializeRepositoryGroups
+  //_initializeRepositoryGroups
   for await (const g of provider.repositoryGroups()) {
     console.log(g);
   }
