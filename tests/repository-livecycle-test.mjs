4e60e0a7fdb5f96e053a5ee8579ae12c96ec2465
Markus Felten <Markus.Felten@gmx.de>
2020-02-05 20:40:43 +0100

    test: check description

--- tests/repository-livecycle-test.mjs
+++ tests/repository-livecycle-test.mjs
@@ -7,11 +7,16 @@ const config = GithubProvider.optionsFromEnvironment(process.env);
 test("create & delete repo", async t => {
   const provider = new GithubProvider(config);
   const group = await provider.repositoryGroup("arlac77");
-  const repo = await group.createRepository("test-repo-1");
+  const repo = await group.createRepository("test-repo-1", {
+    description: "a description"
+  });
   t.truthy(repo);
+  
+  //await repo.createBranch('master');
+
+  t.is(repo.description, "a description");
 
   try {
-  await repo.delete();
-  }
-  catch {}
+    await repo.delete();
+  } catch {}
 });
