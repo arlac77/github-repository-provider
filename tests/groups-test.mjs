53bafb9e0c4fbd1c6deb91f506de63c955d9e01a
Markus Felten <Markus.Felten@gmx.de>
2020-04-03 20:48:54 +0200

    test: groupTest

--- tests/groups-test.mjs
+++ tests/groups-test.mjs
@@ -8,11 +8,11 @@ const config = GithubProvider.optionsFromEnvironment(process.env);
 test(groupListTest, new GithubProvider(config), undefined, { arlac77: {} });
 test(groupListTest, new GithubProvider(config), "*", { arlac77: {} });
 test(groupListTest, new GithubProvider(config), "arlac77", { arlac77: {} });
-test.skip(groupListTest, new GithubProvider(config), "arlac77/uti", { arlac77: {} });
+test.skip(groupListTest, new GithubProvider(config), "arlac77/uti", {
+  arlac77: {}
+});
 test(groupListTest, new GithubProvider(config), "xarlac77", undefined);
-test.skip(groupListTest, new GithubProvider(config), "k0nsti/konsum", { k0nsti: {} });
 
-
-
-test(groupTest, new GithubProvider(config), "arlac77", { arlac77: {} });
+test(groupTest, new GithubProvider(config), "arlac77", { name: "arlac77" });
 test(groupTest, new GithubProvider(config), "xarlac77", undefined);
+test(groupTest, new GithubProvider(config), "k0nsti", { name: "k0nsti" });
