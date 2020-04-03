10e1b285a8f0cbeb66351508a80f9fc5a1925740
Markus Felten <Markus.Felten@gmx.de>
2020-04-03 20:37:06 +0200

    test: use groupTest

--- tests/groups-test.mjs
+++ tests/groups-test.mjs
@@ -1,5 +1,5 @@
 import test from "ava";
-import { groupListTest } from "repository-provider-test-support";
+import { groupListTest, groupTest } from "repository-provider-test-support";
 
 import { GithubProvider } from "../src/github-provider.mjs";
 
@@ -8,4 +8,11 @@ const config = GithubProvider.optionsFromEnvironment(process.env);
 test(groupListTest, new GithubProvider(config), undefined, { arlac77: {} });
 test(groupListTest, new GithubProvider(config), "*", { arlac77: {} });
 test(groupListTest, new GithubProvider(config), "arlac77", { arlac77: {} });
+test.skip(groupListTest, new GithubProvider(config), "arlac77/uti", { arlac77: {} });
 test(groupListTest, new GithubProvider(config), "xarlac77", undefined);
+test.skip(groupListTest, new GithubProvider(config), "k0nsti/konsum", { k0nsti: {} });
+
+
+
+test(groupTest, new GithubProvider(config), "arlac77", { arlac77: {} });
+test(groupTest, new GithubProvider(config), "xarlac77", undefined);
