f74e8c7a5b6743f5c4d0ad82b7b9c2f12dfd1d68
Markus Felten <Markus.Felten@gmx.de>
2020-04-04 03:34:34 +0200

    test(repositoryListTest): cleanups

--- tests/repository-list-test.mjs
+++ tests/repository-list-test.mjs
@@ -4,67 +4,26 @@ import { repositoryListTest } from "repository-provider-test-support";
 import { GithubProvider } from "../src/github-provider.mjs";
 
 const config = GithubProvider.optionsFromEnvironment(process.env);
-
 const provider = new GithubProvider(config);
+
 test(repositoryListTest, provider, "arlac77/npm-*", {
-  "npm-template-sync": { name: "npm-template-sync" }
+  "npm-template-sync": {
+    fullName: "arlac77/npm-template-sync",
+    name: "npm-template-sync",
+  },
+});
+
+test(repositoryListTest, provider, "xtzrtrhtl/npm-*");
+test(repositoryListTest, provider, "arlac77/*", 50);
+
+test(repositoryListTest, provider, "k0nsti/konsum*", {
+  konsum: { fullName: "k0nsti/konsum", name: "konsum" },
 });
 
 test.skip(repositoryListTest, provider, "*", {
-  "npm-template-sync": { name: "npm-template-sync" }
+  "npm-template-sync": { name: "npm-template-sync" },
 });
 
 test.skip(repositoryListTest, provider, undefined, {
-  "npm-template-sync": { name: "npm-template-sync" }
-});
-
-test("list repositories more", async t => {
-  const provider = new GithubProvider(config);
-
-  const branches = {};
-
-  for await (const r of provider.repositories("arlac77/*")) {
-    branches[r.name] = await r.defaultBranch;
-  }
-
-  //console.log(Object.keys(branches));
-  t.truthy(Object.keys(branches).length > 50);
-});
-
-test("list repositories non owner", async t => {
-  const provider = new GithubProvider(config);
-
-  const repositories = {};
-
-  for await (const r of provider.repositories("k0nsti/konsum*")) {
-    repositories[r.fullName] = r;
-  }
-
-  t.truthy(Object.keys(repositories).length > 1);
-  t.truthy(repositories["k0nsti/konsum"].name, "konsum");
-});
-
-test("list repositories negative group", async t => {
-  const provider = new GithubProvider(config);
-
-  const repositories = {};
-
-  for await (const r of provider.repositories("xtzrtrhtl/npm-*")) {
-    repositories[r.name] = r;
-  }
-
-  t.truthy(Object.keys(repositories).length === 0);
-});
-
-test.skip("list repositories all", async t => {
-  const provider = new GithubProvider(config);
-
-  const repositories = {};
-
-  for await (const r of provider.repositories("*/*")) {
-    repositories[r.name] = r;
-  }
-  console.log(Object.keys(repositories));
-
-  t.truthy(Object.keys(repositories).length === 0);
+  "npm-template-sync": { name: "npm-template-sync" },
 });
