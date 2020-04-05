d627e4d1853c4c0f22fd2ab45d2ca1bb5deb1062
Markus Felten <Markus.Felten@gmx.de>
2020-04-05 17:32:17 +0200

    test: make use of branchListTest

--- tests/branch-list-test.mjs
+++ tests/branch-list-test.mjs
@@ -1,16 +1,10 @@
 import test from "ava";
+import { branchListTest } from "repository-provider-test-support";
+
 import { GithubProvider } from "../src/github-provider.mjs";
 
 const config = GithubProvider.optionsFromEnvironment(process.env);
 
-test("list branches", async t => {
-  const provider = new GithubProvider(config);
-
-  const branches = {};
-
-  for await (const b of provider.branches("arlac77/npm-*")) {
-    branches[b.fullCondensedName] = b;
-  }
-
-  t.truthy(Object.keys(branches).length > 3);
-});
+const provider = new GithubProvider(config);
+test(branchListTest, provider, "arlac77/npm-*", 5);
+test(branchListTest, provider, "arlac77/unknown-*");
