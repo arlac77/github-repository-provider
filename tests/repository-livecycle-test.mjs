e623c862c9f43a9fe48b781424ed613bbffeb952
Markus Felten <Markus.Felten@gmx.de>
2020-04-29 13:00:08 +0200

    test: delete tmp repo before test

--- tests/repository-livecycle-test.mjs
+++ tests/repository-livecycle-test.mjs
@@ -5,14 +5,27 @@ import { GithubProvider } from "../src/github-provider.mjs";
 const config = GithubProvider.optionsFromEnvironment(process.env);
 const provider = new GithubProvider(config);
 
-test("create & delete repo", async t => {
+test.skip("create & delete repo", async t => {
   const group = await provider.repositoryGroup("arlac77");
-  const repo = await group.createRepository("test-repo-1", {
+
+  const repoName = "test-repo-1";
+
+  try {
+    const repo = await group.repository(repoName);
+    if(repo) {
+      console.log(repo);
+      await repo.delete();
+    }
+  } catch (e) {
+    console.log(e);
+  }
+
+  const repo = await group.createRepository(repoName, {
     description: "a description",
     auto_init: true
   });
   t.truthy(repo);
-  
+
   t.is(repo.description, "a description");
 
   try {
