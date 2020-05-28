1b60c0ee2efabbd97eb314b8f28fbdd513f2bc97
Markus Felten <Markus.Felten@gmx.de>
2020-05-28 17:58:22 +0200

    fix: use Owner.type from base class

--- src/github-owner.mjs
+++ src/github-owner.mjs
@@ -5,17 +5,6 @@ import { GithubMixin } from "./github-mixin.mjs";
  *
  */
 export class GithubOwner extends GithubMixin(RepositoryGroup) {
-  static get defaultOptions() {
-    return {
-      ...super.defaultOptions,
-      /**
-       * type of the repository group either User or Organization.
-       * @return {string}
-       */
-      type: undefined
-    };
-  }
-
   async createRepository(name, options) {
     // todo check that group is current auth user
     const response = await this.octokit.repos.createForAuthenticatedUser({
