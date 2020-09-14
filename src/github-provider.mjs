85c8aa7b4b216a56d27a669bf0b8445439bfed9a
Markus Felten <Markus.Felten@gmx.de>
2020-09-15 00:02:32 +0200

    fix: make use of fetch-rate-limit-util

--- src/github-provider.mjs
+++ src/github-provider.mjs
@@ -1,5 +1,6 @@
 import fetch from "node-fetch";
 import { replaceWithOneTimeExecutionMethod } from "one-time-execution-method";
+import { rateLimitHandler } from "fetch-rate-limit-util";
 
 import { MultiGroupProvider } from "repository-provider";
 import { GithubRepository } from "./github-repository.mjs";
@@ -145,43 +146,3 @@ replaceWithOneTimeExecutionMethod(
 );
 
 export default GithubProvider;
-
-async function rateLimitHandler(fetcher,queryWait = (msecs,nthTry) => nthTry < 5 ? msecs + 10 : -1) {
-  let response;
-
-  for (let i = 0;; i++) {
-    response = await fetcher();
-
-    switch (response.status) {
-      default:
-        return response;
-
-      case 403:
-        // https://developer.github.com/v3/#rate-limiting
-
-        const remainingRateLimit = parseInt(
-          response.headers.get("x-ratelimit-remaining")
-        );
-
-        const resetRateLimit = parseInt(
-          response.headers.get("x-ratelimit-reset")
-        );
-
-        let millisecondsToWait =
-          new Date(resetRateLimit * 1000).getTime() - Date.now();
-
-        console.log(
-          "x-ratelimit-remaining",
-          remainingRateLimit,
-          resetRateLimit,
-          millisecondsToWait / 1000
-        );
-
-        millisecondsToWait = queryWait(millisecondsToWait, i, response);
-        if(millisecondsToWait <= 0) { return response; }
-        console.log(`wait ${millisecondsToWait / 1000} (${response.url}) ...`);
-        await new Promise(resolve => setTimeout(resolve, millisecondsToWait));
-    }
-  }
-  return response;
-}
