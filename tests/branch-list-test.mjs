101d6dcd9afdba3dbcc5d80f750165d9ba0b14ac
Markus Felten <Markus.Felten@gmx.de>
2020-11-02 03:03:54 +0100

    test: prepare list full name

--- tests/branch-list-test.mjs
+++ tests/branch-list-test.mjs
@@ -1,10 +1,11 @@
 import test from "ava";
 import { branchListTest } from "repository-provider-test-support";
-
 import GithubProvider from "github-repository-provider";
 
-const config = GithubProvider.optionsFromEnvironment(process.env);
+const provider = new GithubProvider(
+  GithubProvider.optionsFromEnvironment(process.env)
+);
 
-const provider = new GithubProvider(config);
 test(branchListTest, provider, "arlac77/npm-*", 5);
+test.skip(branchListTest, provider, "https://gihub.com/arlac77/npm-*", 5);
 test(branchListTest, provider, "arlac77/unknown-*");
