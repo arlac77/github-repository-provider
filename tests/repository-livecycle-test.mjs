d9eb3defeae61508096784c1dc02f2f1f80a1108
Markus Felten <Markus.Felten@db.com>
2020-11-03 11:53:12 +0100

    test: log progress

--- tests/repository-livecycle-test.mjs
+++ tests/repository-livecycle-test.mjs
@@ -8,6 +8,8 @@ const provider = new GithubProvider(config);
 test("create & delete repo", async t => {
   const group = await provider.repositoryGroup("arlac77");
 
+  t.log("group found");
+ 
   const repoName = "test-repo-1";
 
   let repo = await group.repository(repoName);
@@ -15,10 +17,14 @@ test("create & delete repo", async t => {
     await repo.delete();
   }
 
+  t.log("repo cleared");
+
   repo = await group.createRepository(repoName, {
     description: "a description",
     auto_init: true
   });
+
+  t.log("repo created");
 
   t.is(repo.name, repoName);
   t.is(repo.description, "a description");
@@ -26,6 +32,6 @@ test("create & delete repo", async t => {
   try {
     await repo.delete();
   } catch(e) {
-    console.log(e);
+    t.log(e);
   }
 });
