a5da655c8101673ccd56f3dd41a045d444e89f10
Markus Felten <Markus.Felten@gmx.de>
2020-11-03 14:35:36 +0100

    test: make use of repositoryLivecycleTest

--- tests/repository-livecycle-test.mjs
+++ tests/repository-livecycle-test.mjs
@@ -1,37 +1,11 @@
 import test from "ava";
-
+import { repositoryLivecycleTest } from "repository-provider-test-support";
 import GithubProvider from "github-repository-provider";
 
-const config = GithubProvider.optionsFromEnvironment(process.env);
-const provider = new GithubProvider(config);
-
-test("create & delete repo", async t => {
-  const group = await provider.repositoryGroup("arlac77");
-
-  t.log("group found");
- 
-  const repoName = "test-repo-1";
-
-  let repo = await group.repository(repoName);
-  if (repo) {
-    await repo.delete();
-  }
-
-  t.log("repo cleared");
-
-  repo = await group.createRepository(repoName, {
-    description: "a description",
-    auto_init: true
-  });
-
-  t.log("repo created");
-
-  t.is(repo.name, repoName);
-  t.is(repo.description, "a description");
-
-  try {
-    await repo.delete();
-  } catch(e) {
-    t.log(e);
-  }
-});
+test("create & delete repo", async t =>
+  repositoryLivecycleTest(
+    t,
+    GithubProvider.initialize(undefined, process.env),
+    "test-repo-1",
+    "arlac77"
+  ));
