760256f01cee73280a9ba7c01d3c877d861f2fdd
Markus Felten <Markus.Felten@gmx.de>
2020-11-22 01:35:32 +0100

    test: new env vars

--- tests/provider-options-test.mjs
+++ tests/provider-options-test.mjs
@@ -2,8 +2,17 @@ import test from "ava";
 import GithubProvider from "github-repository-provider";
 
 test("provider env options", async t => {
-  t.deepEqual(GithubProvider.optionsFromEnvironment({ GH_TOKEN: "abc" }), {
-    "authentication.type": "token",
-    "authentication.token": "abc"
-  });
+  t.deepEqual(
+    GithubProvider.optionsFromEnvironment({
+      GH_TOKEN: "abc",
+      GITHUB_SERVER_URL: "https://mygihut.com",
+      GITHUB_API_URL: "https://mygihut.com/api/v3"
+    }),
+    {
+      "authentication.type": "token",
+      "authentication.token": "abc",
+      url: "https://mygihut.com",
+      api: "https://mygihut.com/api/v3"
+    }
+  );
 });
