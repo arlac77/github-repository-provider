3d76f867a2d739ea12d424369f76d99b36d54ad8
Markus Felten <Markus.Felten@gmx.de>
2024-08-01 18:32:29 +0100

    test: reformat

--- tests/repository-list-ava.mjs
+++ tests/repository-list-ava.mjs
@@ -1,5 +1,8 @@
 import test from "ava";
-import { repositoryListTest, createMessageDestination } from "repository-provider-test-support";
+import {
+  repositoryListTest,
+  createMessageDestination
+} from "repository-provider-test-support";
 import GithubProvider from "github-repository-provider";
 
 const messageDestination = createMessageDestination().messageDestination;
@@ -21,7 +24,10 @@ test(repositoryListTest, provider, "konsumation/konsum*", {
 
 test(repositoryListTest, provider, "*", {
   "arlac77/template-github": { isTemplate: true },
-  "arlac77/repository-provider": { isArchived: false, name: "repository-provider" },
+  "arlac77/repository-provider": {
+    isArchived: false,
+    name: "repository-provider"
+  },
   "Kronos-Integration/service": { name: "service" },
   "Kronos-Tools/npm-package-template-minimal": { isArchived: true }
 });
@@ -30,6 +36,11 @@ test(repositoryListTest, provider, undefined, {
   "arlac77/repository-provider": { name: "repository-provider" }
 });
 
-test.skip(repositoryListTest, provider, undefined, {
-  "git+https://github.com/template-tools/template-sync-cli.git": { name: "template-sync-cli" }
-});
+test(
+  repositoryListTest,
+  provider,
+  "git+https://github.com/template-tools/template-sync-cli.git",
+  {
+    "template-tools/template-sync-cli": { name: "template-sync-cli" }
+  }
+);
